6c0d0c0bc790a0767c517db7964d12a246b18408
jatin2000416kumar <jatin_2301ec@iitp.ac.in>
2025-03-07 01:11:18 +0530

    added student dashboard and login page

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MentorPlatform from "../Pages/Landing"; 
-import SignupPage from "../Pages/signup";
+import MentorPlatform from "../Pages/Landing"; // Landing Page
+import LoginPage from "../Pages/signup"; // Login Page
+import StudentDashboard from "../Pages/student_dashboard"; // Student Dashboard Page
 
 const AppRouter: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MentorPlatform />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/student_dashboard" element={<StudentDashboard />} />
       </Routes>
     </Router>
   );
 };
 
 export default AppRouter;
+
