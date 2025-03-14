a17c0c7633c20b9421b5d149ba65c75806c9a3cc
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2025-03-15 00:51:58 +0530

    refactor logout functionality to use navigate after logout

--- src/Pages/logout.tsx
+++ src/Pages/logout.tsx
@@ -1,11 +1,18 @@
 import { useAuthStore } from "@/store/authStore";
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Logoutpage: React.FC = () => {
   const { logout } = useAuthStore()
-    
+  let navigate = useNavigate();
+  
+  const logoutHandler = async() => {
+    await logout();
+    navigate('/');
+  }
+
   useEffect(() => {
-    logout();
+    logoutHandler()
   }, [])
 
   return (
